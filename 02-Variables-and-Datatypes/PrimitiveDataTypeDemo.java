4e2c53774d6df8e24d53aea9ed9104ca90807d78
Prathvi <16prithvi@gmail.com>
2026-07-02 09:54:25 +0530

    Add PrimitiveDataTypesDemo class to demonstrate types

---
+++ 02-Variables-and-Datatypes/PrimitiveDataTypeDemo.java
@@ -0,0 +1,23 @@
+public class PrimitiveDataTypesDemo {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 20000;
+        int i = 500000;
+        long l = 9000000000L;
+
+        float f = 5.75f;
+        double d = 19.99;
+
+        char ch = 'J';
+        boolean isJavaFun = true;
+
+        System.out.println("Byte value: " + b);
+        System.out.println("Short value: " + s);
+        System.out.println("Int value: " + i);
+        System.out.println("Long value: " + l);
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+        System.out.println("Char value: " + ch);
+        System.out.println("Boolean value: " + isJavaFun);
+    }
+}
