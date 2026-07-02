9b47809c81ab692172cdcd370286bff8f561d41a
Prathvi <16prithvi@gmail.com>
2026-07-02 09:52:27 +0530

    Update student name and age in StudentInfo.java

---
+++ 02-Variables-and-Datatypes/StudentInfo.java
@@ -0,0 +1,14 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String name = "Somya";
+        int age = 20;
+        char section = 'A';
+        double marks = 88.5;
+
+        System.out.println("Student Information");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Section: " + section);
+        System.out.println("Marks: " + marks);
+    }
+}
