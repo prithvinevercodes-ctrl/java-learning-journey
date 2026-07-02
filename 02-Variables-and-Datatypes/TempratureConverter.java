5f8043d5fbf5ffb2ae14a8d355b97368f5a86718
Prathvi <16prithvi@gmail.com>
2026-07-02 09:58:07 +0530

    Added temprature converter to understand variables and data types

---
+++ 02-Variables-and-Datatypes/TempratureConverter.java
@@ -0,0 +1,9 @@
+public class TemperatureConverter {
+    public static void main(String[] args) {
+        double celsius = 37;
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("Temperature in Celsius: " + celsius);
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+    }
+}
