ace43b11f0cafd5edf52840179706a137f7ac1b7
Prathvi <16prithvi@gmail.com>
2026-07-03 12:16:12 +0530

    Add RelationalOperatorsDemo class to demonstrate operators

---
+++ 03-Operators/RelationalOperatorsDemo.java
@@ -0,0 +1,14 @@
+public class RelationalOperatorsDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Relational Operators");
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+    }
+}
