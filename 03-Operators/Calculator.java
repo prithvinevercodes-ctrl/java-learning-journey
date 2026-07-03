e17f546b9847e264649540043ec173c0bc394f72
Prathvi <16prithvi@gmail.com>
2026-07-03 12:21:08 +0530

    Update calculator output format for clarity

---
+++ 03-Operators/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 4;
+
+        System.out.println("---Simple Calculator---");
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
+        System.out.println("Remainder: " + (num1 % num2));
+    }
+}
