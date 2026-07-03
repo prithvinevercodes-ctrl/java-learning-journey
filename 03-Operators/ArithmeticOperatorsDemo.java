9c6118d87c1a9a0ae3dcae720c4613da91b646ac
Prathvi <16prithvi@gmail.com>
2026-07-03 12:15:12 +0530

    Add ArithmeticOperatorsDemo class for basic operations

---
+++ 03-Operators/ArithmeticOperatorsDemo.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperatorsDemo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Arithmetic Operators");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
