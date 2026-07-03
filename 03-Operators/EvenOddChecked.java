a6f300d745752289babccfbc37950a9c2ca6dab1
Prathvi <16prithvi@gmail.com>
2026-07-03 12:22:00 +0530

    Add EvenOddChecker class to determine even or odd

---
+++ 03-Operators/EvenOddChecked.java
@@ -0,0 +1,12 @@
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        int number = 17;
+
+        System.out.println("Even or Odd Checker");
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}
