491312c8334bf2cc3fbd50f8bacb69f0c2c4148d
Prathvi <16prithvi@gmail.com>
2026-07-03 12:22:50 +0530

    Add SwapNumbers class to swap two integers

---
+++ 03-Operators/SwapNumbers.java
@@ -0,0 +1,18 @@
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Before Swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        int temp = a; // using temporary variable 
+        a = b;
+        b = temp;
+
+        System.out.println("After Swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
