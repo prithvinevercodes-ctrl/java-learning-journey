e53436dca5faf88a52c79cb173f2244bfe0fcfca
Prathvi <16prithvi@gmail.com>
2026-07-04 10:18:54 +0530

    Implement Else-If ladder in ElseIfLadderDemo

---
+++ 04-Conditional-Statements/ElseIfLadder.java
@@ -0,0 +1,14 @@
+public class ElseIfLadderDemo {
+    public static void main(String[] args) {
+        int number = 0;
+
+        System.out.println("Else-If Ladder Demo");
+        if (number > 0) {
+            System.out.println(number + " is positive");
+        } else if (number < 0) {
+            System.out.println(number + " is negative");
+        } else {
+            System.out.println("Number is zero");
+        }
+    }
+}
