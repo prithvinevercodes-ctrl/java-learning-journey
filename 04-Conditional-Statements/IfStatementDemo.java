d578c66c7e87fd9132a49b9e1360dbd9634fc03b
Prathvi <16prithvi@gmail.com>
2026-07-04 10:17:30 +0530

    Add IfStatementDemo class with positive number check

---
+++ 04-Conditional-Statements/IfStatementDemo.java
@@ -0,0 +1,10 @@
+public class IfStatementDemo {
+    public static void main(String[] args) {
+        int number = 10;
+
+        System.out.println("If Statement Demo");
+        if (number > 0) {
+            System.out.println(number + " is positive");
+        }
+    }
+}
