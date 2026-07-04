090958f56534178af09d27e29a8e0c81876c5427
Prathvi <16prithvi@gmail.com>
2026-07-04 10:18:18 +0530

    Add IfElseDemo class for conditional statements

---
+++ 04-Conditional-Statements/IfElseDemo.java
@@ -0,0 +1,12 @@
+public class IfElseDemo {
+    public static void main(String[] args) {
+        int number = -5;
+
+        System.out.println("If-Else Demo");
+        if (number >= 0) {
+            System.out.println(number + " is zero or positive");
+        } else {
+            System.out.println(number + " is negative");
+        }
+    }
+}
