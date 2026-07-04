c2297b02adb1ab21885c0ee5c5fcef1b6fb10b71
Prathvi <16prithvi@gmail.com>
2026-07-04 10:26:40 +0530

    Add VotingEligibilityChecker to determine voting eligibility

---
+++ 04-Conditional-Statements/VotingEligibilityChecker.java
@@ -0,0 +1,12 @@
+public class VotingEligibilityChecker {
+    public static void main(String[] args) {
+        int age = 19;
+
+        System.out.println("Voting Eligibility Checker");
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+}
