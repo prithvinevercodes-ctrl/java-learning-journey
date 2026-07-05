ca1a029fb1a327a8095554912614cad6c881cc64
Prathvi <16prithvi@gmail.com>
2026-07-05 10:58:21 +0530

    Add WhileLoopDemo class with a simple while loop

---
+++ 05-Loops/WhileLoopDemo.java
@@ -0,0 +1,11 @@
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+        System.out.println("While Loop Demo");
+
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("i = " + i);
+            i++;
+        }
+    }
+}
