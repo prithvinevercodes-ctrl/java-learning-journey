f93b73371e1e56c78a610a4675d8659fd4700553
Prathvi <16prithvi@gmail.com>
2026-07-05 10:57:58 +0530

    Add ForLoopDemo class with a simple for loop

---
+++ 05-Loops/ForLoopDemo.java
@@ -0,0 +1,9 @@
+public class ForLoopDemo {
+    public static void main(String[] args) {
+        System.out.println("For Loop Demo");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
