46755d3c6dbb5e11f2525b382a1081029d4d22c1
Prathvi <16prithvi@gmail.com>
2026-07-05 11:00:44 +0530

    Add Enhanced For Loop demo in EmhancedForLoopDemo.java

---
+++ 05-Loops/EmhancedForLoopDemo.java
@@ -0,0 +1,11 @@
+public class EnhancedForLoopDemo {
+    public static void main(String[] args) {
+        System.out.println("Enhanced For Loop (for-each) Demo");
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int num : numbers) {
+            System.out.println("Number = " + num);
+        }
+    }
+}
