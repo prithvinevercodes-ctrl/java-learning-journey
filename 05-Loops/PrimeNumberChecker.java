e33d53a547ad2575f62ca42181d9e1b6d53b7556
Prathvi <16prithvi@gmail.com>
2026-07-05 11:04:28 +0530

    Add PrimeNumberChecker class to check prime numbers

---
+++ 05-Loops/PrimeNumberChecker.java
@@ -0,0 +1,24 @@
+public class PrimeNumberChecker {
+    public static void main(String[] args) {
+        int number = 17;
+        boolean isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= number / 2; i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Prime Number Checker");
+        if (isPrime) {
+            System.out.println(number + " is a prime number");
+        } else {
+            System.out.println(number + " is not a prime number");
+        }
+    }
+}
