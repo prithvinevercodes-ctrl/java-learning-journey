28e547ad2f88d6db8825dc2024e33361683bb41f
Prathvi <16prithvi@gmail.com>
2026-07-05 11:00:12 +0530

    Add DoWhileLoopDemo class for loop demonstration

---
+++ 05-Loops/DoWhileLoopDemo.java
@@ -0,0 +1,11 @@
+public class DoWhileLoopDemo {
+    public static void main(String[] args) {
+        System.out.println("Do-While Loop Demo");
+
+        int i = 1;
+        do {
+            System.out.println("i = " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
