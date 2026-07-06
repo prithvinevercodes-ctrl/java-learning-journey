c2a79d1e4f1313d8f8a0b0e7a0341f7c2fee57d8
Prathvi <16prithvi@gmail.com>
2026-07-06 10:17:30 +0530

    Implement basic calculator using methods

---
+++ 06-Methods/CalculatorUsingMethods.java
@@ -0,0 +1,30 @@
+public class CalculatorUsingMethods {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    static int divide(int a, int b) {
+        return a / b;  // assume b is not zero for now
+    }
+
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 5;
+
+        System.out.println("Calculator Using Methods");
+
+        System.out.println("Addition: " + add(x, y));
+        System.out.println("Subtraction: " + subtract(x, y));
+        System.out.println("Multiplication: " + multiply(x, y));
+        System.out.println("Division: " + divide(x, y));
+    }
+}
