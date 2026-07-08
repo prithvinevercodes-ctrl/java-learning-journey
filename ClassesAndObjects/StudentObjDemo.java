513634ffe8e4b02d00c1138b3880a122560aab1c
Prathvi <16prithvi@gmail.com>
2026-07-08 13:16:07 +0530

    Add StudentDemo class to demonstrate Student object

---
+++ ClassesAndObjects/StudentObjDemo.java
@@ -0,0 +1,12 @@
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+
+        s1.rollNo = 101;
+        s1.name = "Somya";
+        s1.course = "BTech";
+        s1.marks = 87.5;
+
+        s1.displayDetails();
+    }
+}
