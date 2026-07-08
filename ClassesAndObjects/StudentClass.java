96f281bb0813f934be312d21e55718975e770900
Prathvi <16prithvi@gmail.com>
2026-07-08 13:15:15 +0530

    Create StudentClass.java

---
+++ ClassesAndObjects/StudentClass.java
@@ -0,0 +1,14 @@
+public class Student {
+    int rollNo;
+    String name;
+    String course;
+    double marks;
+
+    void displayDetails() {
+        System.out.println("---Student Details---");
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Name: " + name);
+        System.out.println("Course: " + course);
+        System.out.println("Marks: " + marks);
+    }
+}
