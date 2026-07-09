cc12e3bf41f5b262dbb67567eaaf5cdbe8024c83
Prathvi <16prithvi@gmail.com>
2026-07-09 11:38:55 +0530

    Add ConstructorDemo class with a default constructor

---
+++ 09-Constructors-and-This-Keyword/ConstructorDemo.java
@@ -0,0 +1,9 @@
+class ConstructorDemo {
+    ConstructorDemo() {
+        System.out.println("Object created using constructor");
+    }
+
+    public static void main(String[] args) {
+        ConstructorDemo obj = new ConstructorDemo();
+    }
+}
