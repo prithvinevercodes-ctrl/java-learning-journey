1eef4064265274676719cec43160c7e7351a05fa
Prathvi <16prithvi@gmail.com>
2026-07-13 13:34:49 +0530

    Create Animal class with sound and eat methods

---
+++ 13-Polymorphism/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+    // Common method for all animals
+    void makeSound() {
+        System.out.println("Animal makes a sound.");
+    }
+
+    // Common method
+    void eat() {
+        System.out.println("Animal is eating.");
+    }
+}
