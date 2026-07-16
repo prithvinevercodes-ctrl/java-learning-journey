c1222baba1aadd2c1bb072ab44963cfab81a0550
Prathvi <16prithvi@gmail.com>
2026-07-16 14:44:46 +0530

    Add MathOperations class with square and cube methods

---
+++ 16-Packages-and-Access-Modifiers/My pack/MathOperation.java
@@ -0,0 +1,14 @@
+package mypack;
+
+// Class containing static methods
+public class MathOperations {
+
+    // Static methods
+    public static int square(int num) {
+        return num * num;
+    }
+
+    public static int cube(int num) {
+        return num * num * num;
+    }
+}
