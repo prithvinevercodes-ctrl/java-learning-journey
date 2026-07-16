29c3864d3a77e933abdb77e0d6d1f007478d68ba
Prathvi <16prithvi@gmail.com>
2026-07-16 14:44:07 +0530

    Add Calculator class with add and subtract methods

---
+++ 16-Packages-and-Access-Modifiers/My pack/calculator.java
@@ -0,0 +1,15 @@
+package mypack;
+
+// Simple calculator class
+public class Calculator {
+
+    // Public method - accessible from anywhere
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Public method
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
