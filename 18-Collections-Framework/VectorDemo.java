5a810fa74ab4287748116a187b6785b57619ee80
Prathvi <16prithvi@gmail.com>
2026-07-18 18:03:16 +0530

    Add VectorDemo class to demonstrate Vector usage

---
+++ 18-Collections-Framework/VectorDemo.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+        // Vector to store cities
+        Vector<String> cities = new Vector<>();
+
+        cities.add("Delhi");
+        cities.add("Mumbai");
+        cities.add("Lucknow");
+
+        System.out.println("Cities in Vector: " + cities);
+    }
+}
