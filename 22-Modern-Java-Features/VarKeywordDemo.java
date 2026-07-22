b0c31dae8d3bee6c7be843ff4d28eaf121dab59f
Prathvi <16prithvi@gmail.com>
2026-07-22 14:22:46 +0530

    Add VarKeywordDemo class to demonstrate var usage

---
+++ 22-Modern-Java-Features/VarKeywordDemo.java
@@ -0,0 +1,15 @@
+public class VarKeywordDemo {
+    public static void main(String[] args) {
+        var name = "Sarthak";
+        var age = 20;
+        var salary = 45000.50;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+
+        // var works with collections too
+        var message = "Learning modern Java";
+        System.out.println(message);
+    }
+}
