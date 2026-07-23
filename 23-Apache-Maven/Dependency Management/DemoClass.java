7ef9922c6718cecf73cad2543f63a455b7b959a8
Prathvi <16prithvi@gmail.com>
2026-07-23 12:24:18 +0530

    Add DemoClass for Maven dependency management example

---
+++ 23-Apache-Maven/Dependency Management/DemoClass.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class DependencyManagementDemo {
+    public static void main(String[] args) {
+        System.out.println("This project uses Maven dependency management.");
+    }
+}
