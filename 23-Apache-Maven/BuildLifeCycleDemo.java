8d3ae60d427240939e06a3e86b6adc1aaabd2fa9
Prathvi <16prithvi@gmail.com>
2026-07-23 12:24:52 +0530

    Add BuildLifecycleDemo class for Maven lifecycle demo
    This class demonstrates the Maven build lifecycle with example commands.

---
+++ 23-Apache-Maven/BuildLifeCycleDemo.java
@@ -0,0 +1,14 @@
+package com.example;
+
+// This class is only for explaining lifecycle practically
+public class BuildLifecycleDemo {
+    public static void main(String[] args) {
+        System.out.println("Maven Build Lifecycle Demo");
+        System.out.println("Run commands like:");
+        System.out.println("mvn validate");
+        System.out.println("mvn compile");
+        System.out.println("mvn test");
+        System.out.println("mvn package");
+        System.out.println("mvn install");
+    }
+}
