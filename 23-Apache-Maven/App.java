5038dda7cbaa0b534681acb77f94457ccb794488
Prathvi <16prithvi@gmail.com>
2026-07-23 12:20:02 +0530

    Add simple starter class for Maven project

---
+++ 23-Apache-Maven/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+// Simple starter class inside a Maven project
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Maven project!");
+    }
+}
