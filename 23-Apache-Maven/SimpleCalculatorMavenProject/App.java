594ea8d7d26f8931cd9c6e87bae88b163c0e710d
Prathvi <16prithvi@gmail.com>
2026-07-23 12:28:31 +0530

    Add main class for calculator application

---
+++ 23-Apache-Maven/SimpleCalculatorMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+// Main class to test calculator
+public class App {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+    }
+}
